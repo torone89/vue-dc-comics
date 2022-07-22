657c83d51ae7cac1f3294440d916615aade4bb97
torone89 <torone89>
2022-07-22 17:19:59 +0200

    Complete SocialFooter

--- src/components/SocialFooter.vue
+++ src/components/SocialFooter.vue
@@ -2,12 +2,13 @@
   <footer class="social-bg">
     <div class="container">
       <div class="social">
+        <!-- BOTTONE -->
         <div class="sign-up">
           <button><a href="#">sign-up now!</a></button>
         </div>
-
+        <!-- SOCIAL ICONS -->
         <div class="social-icons">
-          <span>follow us</span>
+          <span class="follow">follow us</span>
           <img src="../assets/img/footer-facebook.png" alt="" />
           <img src="../assets/img/footer-twitter.png" alt="" />
           <img src="../assets/img/footer-youtube.png" alt="" />
@@ -29,5 +30,59 @@ export default {
 <style scoped>
 .social-bg {
   background-color: #2f2f2f;
+}
+
+.social {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 30px;
+}
+.social-icons {
+  display: flex;
+  justify-content: space-around;
+}
+
+.sign-up {
+  margin-left: 5px;
+}
+
+/* BUTTON */
+button {
+  border: none;
+  border: 2px solid #1e86ed;
+  padding: 17px;
+  background-color: #2f2f2f;
+}
+
+/* ALINK */
+a {
+  text-transform: uppercase;
+  font-weight: bold;
+  color: white;
+  font-size: 15px;
+}
+
+/* IMG */
+img {
+  padding: 10px;
+  width: 50px;
+  height: 50px;
+  cursor: pointer;
+}
+
+img:hover {
+  background-color: white;
+  border-radius: 50px 50px;
+}
+
+/* SPAN FOLLOW */
+.follow {
+  text-transform: uppercase;
+  color: #1e86ed;
+  font-weight: bold;
+  font-size: 20px;
+  margin: 15px 10px;
+  cursor: pointer;
 }
 </style>
