e3748ed478fda43b3ef08d54cb7481e0012a3e0e
torone89 <torone89>
2022-07-22 16:21:54 +0200

    Footer nav Complete Problema Immagine

--- src/components/BaseFooter.vue
+++ src/components/BaseFooter.vue
@@ -1,5 +1,55 @@
 <template>
-  <header>ciccio</header>
+  <footer>
+    <div class="container">
+      <nav>
+        <div class="nav-left">
+          <ul>
+            <li><h3>dc comics</h3></li>
+            <li><a href="#">Characters</a></li>
+            <li><a href="#">Comics</a></li>
+            <li><a href="#">Movies</a></li>
+            <li><a href="#">Tv</a></li>
+            <li><a href="#">Games</a></li>
+            <li><a href="#">Videos</a></li>
+            <li><a href="#">News</a></li>
+            <li><h3>shop</h3></li>
+            <li><a href="#">Shop DC</a></li>
+            <li><a href="#">Shop DC Collectibles</a></li>
+          </ul>
+
+          <ul>
+            <li><h3>dc</h3></li>
+            <li><a href="#">Terms Of Use</a></li>
+            <li><a href="#">Privacy policy (New)</a></li>
+            <li><a href="#">Ad Choices</a></li>
+            <li><a href="#">Advertising</a></li>
+            <li><a href="#">Jobs</a></li>
+            <li><a href="#">Subscriptions</a></li>
+            <li><a href="#">Talent Workshops</a></li>
+            <li><a href="#">CPSC Certificates</a></li>
+            <li><a href="#">Ratings</a></li>
+            <li><a href="#">Shop Help</a></li>
+            <li><a href="#">Contact Us</a></li>
+          </ul>
+
+          <ul>
+            <li><h3>sites</h3></li>
+            <li><a href="#">DC</a></li>
+            <li><a href="#">MAD Magazine</a></li>
+            <li><a href="#">DC Kids</a></li>
+            <li><a href="#">DC Universe</a></li>
+            <li><a href="#">DC Power Visa</a></li>
+          </ul>
+        </div>
+
+        <div class="nav-right">
+          <figure>
+            <img src="../assets/img/dc-logo-bg.png" alt="" />
+          </figure>
+        </div>
+      </nav>
+    </div>
+  </footer>
 </template>
 
 <script>
@@ -10,4 +60,42 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+footer {
+  background-image: url("../assets/img/footer-bg.jpg");
+  background-size: cover;
+}
+
+nav {
+  display: flex;
+  justify-content: space-between;
+}
+
+.nav-left {
+  display: flex;
+  padding: 40px 0;
+}
+
+li {
+  margin-right: 30px;
+  line-height: 30px;
+}
+
+h3 {
+  text-transform: uppercase;
+  font-size: 19px;
+  color: #fdf0ff;
+}
+a {
+  text-decoration: none;
+  color: grey;
+}
+
+.nav-right {
+  max-height: 200px;
+}
+
+img {
+}
 </style>
+
+
--- src/components/TheMain.vue
+++ src/components/TheMain.vue
@@ -1,9 +1,38 @@
 <template>
-  <header>
-    <div class="container">
-      <h1>Content Goes Here</h1>
-    </div>
-  </header>
+  <main>
+    <header>
+      <div class="container">
+        <h1>Content Goes Here</h1>
+      </div>
+    </header>
+
+    <section class="shop">
+      <div class="container">
+        <ul>
+          <li>
+            <img src="../assets/img/buy-comics-digital-comics.png" alt="" />
+            <span>digital comics</span>
+          </li>
+          <li>
+            <img src="../assets/img/buy-comics-merchandise.png" alt="" />
+            <span>dc merchandise</span>
+          </li>
+          <li>
+            <img src="../assets/img/buy-comics-subscriptions.png" alt="" />
+            <span>subscription</span>
+          </li>
+          <li>
+            <img src="../assets/img/buy-comics-shop-locator.png" alt="" />
+            <span>comic shop locator</span>
+          </li>
+          <li>
+            <img src="../assets/img/buy-dc-power-visa.svg" alt="" />
+            <span>dc power visa</span>
+          </li>
+        </ul>
+      </div>
+    </section>
+  </main>
 </template>
 
 <script>
@@ -14,6 +43,7 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+/* HEADER */
 header {
   background-color: black;
   padding: 50px 0;
@@ -23,5 +53,28 @@ h1 {
   color: white;
   font-weight: 500;
   font-size: 24px;
+}
+
+/* SEcTION ShOP */
+.shop {
+  background-color: #1e86ed;
+  padding: 50px 0;
+}
+
+.container ul {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  text-transform: uppercase;
+}
+.container ul li {
+  display: flex;
+  align-items: center;
+  color: white;
+}
+
+img {
+  width: 40px;
+  margin-right: 15px;
 }
 </style>
